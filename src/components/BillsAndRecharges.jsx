a87ca78969ad969f18b77caa90a6001d91d96d4e
aviralnx <aviral.swarnkar@successive.tech>
2024-06-03 20:54:36 +0530

    feat: update bills component

--- src/components/BillsAndRecharges.jsx
+++ src/components/BillsAndRecharges.jsx
@@ -1,42 +1,23 @@
+import { createSignal } from 'solid-js';
 import styles from './component.module.css';
 
 const BillsAndRecharges = () => {
+  const [items] = createSignal([
+    { id: 1, src: 'https://cms-assets.bajajfinserv.in/is/image/bajajfinancestage/mobile-recharge-3?scl=1&amp;fmt=png-alpha', text: 'Mobile' },
+    { id: 2, src: 'https://cms-assets.bajajfinserv.in/is/image/bajajfinancestage/electricity-22?scl=1&amp;fmt=png-alpha', text: 'Electricity' },
+    { id: 3, src: 'https://cms-assets.bajajfinserv.in/is/image/bajajfinancestage/dth-recharge?scl=1&amp;fmt=png-alpha', text: 'DTH' },
+    { id: 4, src: 'https://cms-assets.bajajfinserv.in/is/image/bajajfinancestage/credit-card-bill?scl=1&fmt=png-alpha', text: 'Credit Card' },
+  ]);
   return (
       <section>
         <h2>Bills and recharges</h2>
         <div class={styles.billsSection}>
-        <a href="/bajajpay/bbps/?categoryId=5">
+        {items().map((item) => <a href="/bajajpay/bbps/?categoryId=5">
           <div class={styles.itemsContainer}>
-            <img src="https://cms-assets.bajajfinserv.in/is/image/bajajfinancestage/mobile-recharge-3?scl=1&amp;fmt=png-alpha" alt="Mobile Recharge" loading="lazy" />
-            <p class="items-wrapper__title fs-14-16">Mobile</p>
+            <img src={item.src} alt={item.text} loading="lazy" />
+            <p class="items-wrapper__title fs-14-16">{item.text}</p>
           </div>
-        </a>
-      
-        <a  href="/bajajpay/bbps/?categoryId=4">
-          <div class={styles.itemsContainer}>
-            <img src="https://cms-assets.bajajfinserv.in/is/image/bajajfinancestage/electricity-22?scl=1&amp;fmt=png-alpha" alt="Electricity" loading="lazy" />
-            
-          <p class="items-wrapper__title fs-14-16">
-            Electricity</p>
-          </div>
-        </a>
-      
-        <a href="/bajajpay/bbps/?categoryId=12">
-          <div class={styles.itemsContainer}>
-            <img src="https://cms-assets.bajajfinserv.in/is/image/bajajfinancestage/dth-recharge?scl=1&amp;fmt=png-alpha" alt="DTH" loading="lazy" />
-            
-          <p class="items-wrapper__title fs-14-16">
-            DTH</p>
-          </div>
-        </a>
-        <a href="/bajajpay/bbps/?categoryId=12">
-          <div class={styles.itemsContainer}>
-            <img src="https://cms-assets.bajajfinserv.in/is/image/bajajfinancestage/credit-card-bill?scl=1&fmt=png-alpha" alt="DTH" loading="lazy" />
-            
-          <p class="items-wrapper__title fs-14-16">
-            Credit Card</p>
-          </div>
-        </a>
+        </a>)}
       </div>
       </section>
   )
